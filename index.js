9bb84c0448a210a1fba46ac057910499b799a214
Damion de Wit <damion.dewit@outlook.com>
2018-05-15 14:36:46 +0200

    CLeanup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Log = require("./log.js");
 var myLog = new Log();
 
 myLog.SetLevel("Info");     // Error, Warning, Info
-myLog.SetFormat("Long")  // Long, Compact
+myLog.SetFormat("Long");    // Long, Compact
 
 myLog.Error("hello");
 myLog.Warn("hello");
